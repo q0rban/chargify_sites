22f89a8d7e7999970e2deccba9733208bde044e2
jsansbury <james@sprocketcreative.com>
2010-03-01 14:19:45 -0500

    Work on product page.

---
+++ chargify-sites-product-page.tpl.php
@@ -0,0 +1,11 @@
+<div id="chargify-sites-product-page">
+  <div class="chargify-sites-product-description">
+    <div class="price">
+      <?php print $price; ?>
+    </div>
+    <p><?php print $description; ?></p>
+  </div>
+  <div class="chargify-sites-product-form">
+    <?php print $form_output; ?>
+  </div>
+</div>
