bf09bacd4728c2ea44eb543863ab32e3d9a93786
jsansbury <james@sprocketcreative.com>
2010-03-03 16:03:24 -0500

    Add fancyForm.js to product and payment pages.

---
+++ fancyForm.js
@@ -0,0 +1,50 @@
+Drupal.behaviors.fancyFormDescriptions = function (context) {
+  $('div.form-item input', context).focus(function () {
+    $(this).parents('div.form-item:first').find('div.description').show('fast');
+  })
+  .blur(function () {
+    $(this).parents('div.form-item:first').find('div.description').hide('fast');
+  })
+  .blur();
+};
+
+Drupal.behaviors.fancyFormLabels = function (context) {
+  var fields = $('input.form-text', context).not('[type=password]');
+  var forms = fields.parents('form:first');
+
+  fields.blur(putLabels).focus(clearLabels).blur();
+  forms.each(function() {
+    $(this).submit(function() {
+      $('input[type=submit]', this).attr('disabled', 'disabled').val("Please wait...");
+      fields.focus();
+    });
+  });
+
+  function putLabels() {
+    var element = $(this);
+    var id = element.attr('id');
+
+    if (element.attr('label')) {
+      var val = element.attr('label');
+    }
+    else {
+      var label = $('label[for=' + id + ']');
+      var val = label.text().split(':');
+      val = val[0];
+    }
+
+    if ((element.val() === this.defaultValue) && (this.size > val.length)) {
+      if (label) {
+        label.hide();
+      }
+      element.val(val).attr('label', val);
+    }
+  }
+
+  function clearLabels() {
+    var element = $(this);
+    if (element.attr('label') == element.val()) {
+      element.val(this.defaultValue);
+    }
+  }
+};
